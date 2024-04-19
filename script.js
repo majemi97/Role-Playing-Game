7b07b7184243aa06632319c06488568591bd2eb6
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 18:01:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -123,8 +123,10 @@ function sellWeapon() {
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
+    }
   }
-}
 
 function fightSlime() {
 
