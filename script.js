d607087a0776d99839b1338ced32e6a83208680d
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 17:26:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -103,6 +103,7 @@ function buyWeapon() {
     goldText.innerText = gold;
     let newWeapon = weapons[currentWeapon].name;
     text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
   }
 }
 
