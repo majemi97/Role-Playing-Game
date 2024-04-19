e4a5541635125989ca812f2e20d6775fa19c981c
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 17:10:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -97,9 +97,13 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30);
-  gold -= 30;
-  currentWeapon++;
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    
+    text.innerText = "You now have a new weapon.";
+  }
 }
 
 function fightSlime() {
