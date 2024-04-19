cb5750c877e89474a907a4c84d58c09955805dc0
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 14:49:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore() {
   button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
 }
 function goCave() {
   console.log("Going to cave.");
