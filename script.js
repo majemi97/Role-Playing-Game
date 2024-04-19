31636ad99d818b3dace2e93789701c7e8155a5de
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 17:43:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -109,9 +109,10 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
   }
 }
-
 function fightSlime() {
 
 }
