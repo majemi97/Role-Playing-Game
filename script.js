7b4dfdb62c1a946ba6069e07d51f32814ba58ddc
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 17:33:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,9 @@ function buyWeapon() {
     let newWeapon = weapons[currentWeapon].name;
     text.innerText = "You now have a " + newWeapon + ".";
     inventory.push(newWeapon);
-    text.innerText += " In your inventory you have: " + inventory;
+    text.innerText += " In your inventory you have: " + inventory
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
   }
 }
 
