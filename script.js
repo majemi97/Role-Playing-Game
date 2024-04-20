125992127b430ba24dc1b3f25d74bf0c6099a0a0
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-04-19 19:37:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -182,11 +182,15 @@ function attack() {
   monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
-  
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
 }
 
 function dodge() {
-  
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
 
 function defeatMonster() {
